5484dce0021b7a9566d5907f8e6046ab940e25c0
etstefans-commits <>
2025-10-21 10:28:13 +0300

    popravka

--- Domasno/Domasno/Program.cs
+++ Domasno/Domasno/Program.cs
@@ -9,42 +9,23 @@ double price = double.Parse(Console.ReadLine());
 
 double discount = 0;
 
-
 if (price < 50)
 {
     discount = 0;
-    price = price - price * discount / 100;
-    Console.WriteLine(name + " " + price);
 }
-else if ((price < 50) && (age < 18))
-{
-    discount = 2;
-    price = price - price * discount / 100;
-    Console.WriteLine(name + " " + price);
-}
-
-if ((price > 50) && (price < 100))
+else if ((price > 50) && (price < 100))
 {
     discount = 5;
-    price = price - price * discount / 100;
-    Console.WriteLine(name + " " + price);
 }
-else if (((price > 50) && (price < 100)) && (age < 18))
-{
-    discount = 7;
-    price = price - price * discount / 100;
-    Console.WriteLine(name + " " + price);
-}
-
-if (price > 100)
+else if (price > 100)
 {
     discount = 10;
-    price = price - price * discount / 100;
-    Console.WriteLine(name + " " + price);
 }
-else if ((price > 100) && (age < 18))
+
+if (age < 18)
 {
-    discount = 12;
-    price = price - price * discount / 100;
-    Console.WriteLine(name + " " + price);
+    discount += 2;
 }
+
+price = price - price * discount / 100;
+Console.WriteLine(name + " " + price);
